59b92726e007abd9afe86d7d626c525fb2638265
glconde <georgelouie.conde@edu.sait.ca>
2025-02-24 07:49:57 -0700

    add comments

--- pages/tenant.js
+++ pages/tenant.js
@@ -6,6 +6,7 @@ import axios from "axios";
 export default function Tenant() {
   const [wallet, setWallet] = useState(1000);
 
+  // start rent use case
   const startContract = () => {
     axios
       .post("http://localhost:5000/start-contract")
@@ -13,6 +14,7 @@ export default function Tenant() {
       .catch((err) => console.error(err));
   };
 
+  // grab wallet data, polls every 2 secs
   useEffect(() => {
     const interval = setInterval(() => {
       axios
